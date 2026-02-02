8e2fecf8522a7836fa9a0ddf148cf2605305e7b3
Sujit Avchar <145844481+sujitavchar@users.noreply.github.com>
2026-02-02 21:53:09 +0530

    Database setup

--- backend/app.js
+++ backend/app.js
@@ -2,9 +2,10 @@
 import express from "express";
 import cors from "cors";
 import 'dotenv/config';
-import {db} from './db.js'
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
+
+export default app;
--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,11 @@
-import pg from 'pg';
+import dotenv from "dotenv";
+import {db} from "./src/db.js";
+import app from "./app.js"
 
-const {Pool} = pg;
-
-export const db = new Pool({
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
+dotenv.config({
+    path: "./.env"
+});
+ 
+app.listen(process.env.PORT, ()=>{
+    console.log(`Server listening at port ${process.env.PORT}`);
 })
